038d52a0d4f2c53ed68a3f2d2f33ba95f015fe2c
Thiago <thiagoonline89@hotmail.com>
2018-10-15 20:59:51 -0300

    Adicionar arquivos de projeto.

---
+++ angular_NgRoute/js/main.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="../lib/angular.js" />
+
+
+var app = angular.module('blog', ['ngRoute']);
+
+//Definir rota
+app.config(function ($routeProvider) {
+
+    $routeProvider
+        .when('/', { templateUrl: '/views/home.html' })
+        .when('/artigos', { templateUrl: '/views/artigos.html', controller: 'ArtigosController' })
+        .when('/sobre', { templateUrl: '/views/sobre.html', controller: 'SobreController' })
+        .otherwise({redirectTo: "/"})
+});
+
+app.controller('ArtigosController', function ($scope) {
+    $scope.artigos = [
+        "VB. net - Números perfeitos - macoratti. net",
+        "VB. net - Fatoração - macoratti. net",
+        "VB. net - Formatando números com ToString() - macoratti. net"
+    ];
+
+})
+
+app.controller('SobreController', function ($scope) {
+    $scope.titulo = 'Sobre';
+    $scope.Nome = 'Macoratti .Net';
+    $scope.sobre = 'Artigos sobre AngularJS';
+});
